2fcc758422bde78d54a60ce8118cb0c5d15684fb
Szymon Nowak <szimek@gmail.com>
2014-08-02 15:53:29 +0200

    Fix

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,5 +1,12 @@
 module RegexpM17N
   def self.non_empty?(str)
-    str =~ /^.+$/
+    pattern  = "^.+$".encode(str.encoding)
+
+    if str.encoding.dummy?
+      str     = str.encode("UTF-8")
+      pattern = pattern.encode("UTF-8")
+    end
+
+    str =~ Regexp.new(pattern)
   end
 end
